9b006aa788063d4883820297c907f33d246e6100
Jun Aishima <jaishima@bnl.gov>
2020-06-18 10:24:55 -0400

    remove everything not related to current Qt EPICS objects
     * server and GUI component prototypes, documentation, CaChannel
       binaries, etc. have been removed

--- toyBL.pyEpics/blCommUtils.py
+++
@@ -1,6 +0,0 @@
-import time
-global message_string_pv
-
-def broadcast_output(s):
-  time.sleep(0.01)
-  message_string_pv.put(s,wait=True)
--- toyJunk/toyGui.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-from Tkinter import *
-from epicsMotorLabel import *
-root = Tk()
-m = epicsMotorLabel("x25a:mon",root,10)
-m.pack()
-epicsMotorLabel.is_drawn = 1
-root.mainloop()
